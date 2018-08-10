a7c633f10097a7717b32c30d5e81d03dd4cf83e9
Paweł Musiał <pawcio16m@gmail.com>
2018-08-10 20:46:22 +0200

    User Database added

--- src/javaApplication/MainApplication.java
+++ src/javaApplication/MainApplication.java
@@ -1,20 +1,19 @@
-/**
- * 
- */
 package javaApplication;
 
-/**
- * @author pmusial
- *
- */
 public class MainApplication {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		System.out.println("Hello World");
+		
+		DatabaseConnection databaseConn = new DatabaseConnection();
+		
+		databaseConn.createTables();
+		//databaseConn.usersTable.insertUser("Pawel", "Musial", 26, "726384938", "pawcio16m@gmail.com", "Kraków");
+		databaseConn.usersTable.printAllUsers();
+		databaseConn.closeConnection();
 	}
 
 }
