979fd972a8d614b58c07d1f3f814e9199463e297
Paweł Musiał <pawcio16m@gmail.com>
2018-08-10 18:32:00 +0200

    Initial commit

---
+++ TestModules/javaApplicationTestModules/JavaApplicationTests.java
@@ -0,0 +1,11 @@
+package javaApplicationTestModules;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UserTestSuite.class })
+public class JavaApplicationTests {
+
+}
---
+++ src/javaApplication/MainApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package javaApplication;
+
+/**
+ * @author pmusial
+ *
+ */
+public class MainApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+
+}
